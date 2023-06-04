7809005247c87db734d94736a1eef7957ef064fc
Alexander <alexvictoria232@gmail.com>
2023-06-04 16:07:35 +0200

    Responsive

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,23 @@
+function myFunction() {
+  var x = document.getElementById(myTopnav);
+  if (x.className === topnav) {
+    x.className +=  responsive;
+  } else {
+    x.className = topnav;
+  }
+}
+
+
+
+
+
+
+
 let layer_1 = document.querySelector('.layer-1')
 let layer_2 = document.querySelector('.layer-2')
 let layer_3 = document.querySelector('.layer-3')
 let layer_4 = document.querySelector('.layer-4')
 let layer_5 = document.querySelector('.layer-5')
-
-
-
 
 
 window.onscroll = function(){
@@ -20,7 +32,7 @@ window.onscroll = function(){
 function verMas(){
     let boton = document.getElementById("boton")
     boton.addEventListener("click", function(){
-        window.location.href = "google.com"
+        window.location.href = ""
     })
 }
 
