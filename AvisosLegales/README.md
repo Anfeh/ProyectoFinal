21ad41cc9b81fe467d0875db4198aa8ce9a04039
Alexander <alexvictoria232@gmail.com>
2023-06-05 16:18:19 +0200

    Lugares aniadidos y avisos legales. Todo responsive

---
+++ AvisosLegales/README.md
@@ -0,0 +1,6 @@
+# ProyectoFinal
+
+Proyecto final de Lenguaje de Marcas.
+
+Crearé una página de exploración de lugares "ocultos" en España, para que todos puedan disfrutar de las increíbles vistas. RECUERDA, siempre dejar el lugar limpio y recoje la basura para poder disfrutar 
+de más lugares como éstos.
---
+++ masLugares/js/scripts.js
@@ -0,0 +1,45 @@
+function myFunction(){
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+      x.className += " responsive";
+  }else{
+      x.className = "topnav";
+  }
+}
+
+
+
+
+
+
+let layer_1 = document.querySelector('.layer-1')
+let layer_2 = document.querySelector('.layer-2')
+let layer_3 = document.querySelector('.layer-3')
+let layer_4 = document.querySelector('.layer-4')
+let layer_5 = document.querySelector('.layer-5')
+
+
+window.onscroll = function(){
+    let y = window.scrollY;
+    layer_1.style.transform = 'translateY('+ y/2 +'px)';
+    layer_2.style.transform = 'translateY('+ y/2 +'px)';
+    layer_3.style.transform = 'translateY('+ y/1.5 +'px)';
+    layer_4.style.transform = 'translateY('+ y/2 +'px)';
+    layer_5.style.transform = 'translateY('+ y/3 +'px)';
+}
+
+function verMas(){
+    let boton = document.getElementById("boton")
+    boton.addEventListener("click", function(){
+        window.location.href = ""
+    })
+}
+
+$(document).ready(function(){ irArriba(); }); //Hacia arriba
+function irArriba(){
+  $('.ir-arriba').click(function(){ $('body,html').animate({ scrollTop:'0px' },1000); });
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 0){ $('.ir-arriba').slideDown(600); }else{ $('.ir-arriba').slideUp(600); }
+  });
+  $('.ir-abajo').click(function(){ $('body,html').animate({ scrollTop:'1000px' },1000); });
+}
